d836844a55b512265c2813686bec649b76d8ebbb
KamyaMudaliar <kamyamudaliar@gmail.com>
2025-06-13 12:09:34 +0530

    Update App.tsx before pulling from origin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Landing } from './pages/Landing';
 import { Dashboard } from './pages/Dashboard';
+import Mentor_session from './pages/Mentor_session';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/mentor-session" element={<Mentor_session />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
